a77cb9dac3b0819c7d9133fdf30496b41717a9b4
Miguel <miguelalvesparente002@gmail.com>
2025-08-05 09:56:01 -0300

    Meu primeiro commit

---
+++ Inicio/dados_2.js
@@ -0,0 +1,11 @@
+console.log(19.90 * 0.6)
+
+let preco = 19.90
+let desconto = 0.4
+let preComDesconto = preco * (1 - desconto)
+
+console.log(preComDesconto)
+
+let nome = "caderno" 
+let categoria = "papelaria"
+ console.log (nome + " " + categoria)
