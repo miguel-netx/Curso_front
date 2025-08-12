12d2950ba4c94a2fd5d0bd167f847d232d843ae8
Miguel <miguelalvesparente002@gmail.com>
2025-08-12 09:05:23 -0300

    Calculando circuferencias e troca de variaveis

---
+++ Inicio/desafio.troca.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 94;
+
+let tempo = a;
+
+a = b;
+b = tempo;
+
+console.log(a);
+console.log(b);
---
+++ Inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const raio = 10;
+const PI = 3.141592;
+let areacirc = PI * raio * raio;
+
+console.log("o valor da circuferencia é de",areacirc,"m2.");
+
+areacirc = Math.PI * raio * raio;
+console.log("o valor da circuferencia é de",areacirc,"m2.");
