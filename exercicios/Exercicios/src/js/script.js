5c078608dd1fc2582a9b3f6e09fb5c3e1b9ee460
Miguel <miguelalvesparente002@gmail.com>
2025-08-18 11:15:08 -0300

    Loop com var/let e conclusão do 1° exercicio

---
+++ exercicios/Exercicios/src/js/script.js
@@ -0,0 +1,16 @@
+let value = document.getElementById("value-count")
+let btnPlus = document.getElementById("btn-plus")
+let btnMinus = document.getElementById("btn-minus")
+let btnReset = document.getElementById("btn-reset")
+
+btnPlus.addEventListener('click', () => {
+    value.innerHTML = parseInt(value.innerHTML) + 1;
+})
+
+btnMinus.addEventListener('click', () => {
+    value.innerHTML = parseInt(value.innerHTML) - 1;
+})
+
+btnReset.addEventListener('click', () => {
+    value.innerHTML = 0;
+})
