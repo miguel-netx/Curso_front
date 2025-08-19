27ce247c3cb7c9c9aefe190978f16b0de3987a02
Miguel <miguelalvesparente002@gmail.com>
2025-08-19 11:29:07 -0300

    Continuação do curso web

---
+++ Inicio/Funcao_x_obejto.js
@@ -0,0 +1,10 @@
+console.log(typeof Object);
+console.log(typeof new Object);
+
+const client = function() {}
+console.log(typeof client);
+console.log(typeof new client);
+
+class produto {}
+console.log(typeof produto);
+console.log(typeof new produto);
