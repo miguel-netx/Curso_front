e4cff7ba40235ccabd39ec0a4b49dec2d4ad94d9
Miguel <miguelalvesparente002@gmail.com>
2025-08-20 11:33:48 -0300

    Aprendendo Operações Logicas

---
+++ Exercicios/exercicio1_curso.js
@@ -0,0 +1,11 @@
+function parOuImpar (numero) {
+    if (numero % 2 === 0) {
+        console.log(numero + " é Par")
+    }
+    else {
+        console.log(numero + " é Impar")
+    }
+}
+
+parOuImpar(10)
+parOuImpar(9)
---
+++ Inicio/opLogicos.js
@@ -0,0 +1,12 @@
+function compras(trabalho1, trabalho2) {
+    const comprartv50p = trabalho1 || trabalho2;
+    const comprartv32p = trabalho1 && trabalho2;
+    const comprarSorvete = trabalho1 != trabalho2;
+    const ficarSaudavel = !comprarSorvete
+
+    return{comprartv50p, comprartv32p, comprarSorvete, ficarSaudavel}
+}
+
+console.log(compras(true, true));
+
+
